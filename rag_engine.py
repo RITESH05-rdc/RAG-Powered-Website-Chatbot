2dc29e92fd3e9f709d96ace781ba03b855a7fd11
*chariritesh6@gmail.com* <144829970+RITESH05-rdc@users.noreply.github.com>
2026-03-10 18:44:42 +0530

    Add semantic search functionality

--- rag_engine.py
+++ rag_engine.py
@@ -19,3 +19,18 @@ def create_vector_store(chunks):
 
     with open("vector_store/chunks.pkl", "wb") as f:
         pickle.dump(chunks, f)
+
+def search(query):
+
+    index = faiss.read_index("vector_store/index.faiss")
+
+    with open("vector_store/chunks.pkl","rb") as f:
+        chunks = pickle.load(f)
+
+    query_vector = model.encode([query])
+
+    D, I = index.search(query_vector, 3)
+
+    results = [chunks[i] for i in I[0]]
+
+    return results
