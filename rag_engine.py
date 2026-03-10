3102dc92b5d6493d01420099a250fa68885ce500
*chariritesh6@gmail.com* <144829970+RITESH05-rdc@users.noreply.github.com>
2026-03-10 18:43:36 +0530

    Implement vector database creation using FAISS

---
+++ rag_engine.py
@@ -0,0 +1,21 @@
+import faiss
+import pickle
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def create_vector_store(chunks):
+
+    embeddings = model.encode(chunks)
+
+    dimension = embeddings.shape[1]
+
+    index = faiss.IndexFlatL2(dimension)
+
+    index.add(np.array(embeddings))
+
+    faiss.write_index(index, "vector_store/index.faiss")
+
+    with open("vector_store/chunks.pkl", "wb") as f:
+        pickle.dump(chunks, f)
